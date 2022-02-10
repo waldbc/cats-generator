ecb4aa420f76df4fc7043012e397427ea17f1c1c
Ryan Ferguson <ryan@raijinn.xyz>
2022-02-10 08:51:47 -0500

    Fix whitespace & add module docstring

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+""" Main process module - Generates layered image and metadata files """
+
 import json
 import os
 import random
@@ -7,14 +9,13 @@ from PIL import Image
 from config import layers
 
 
-ASSETS = os.path.join(os.getcwd(), 'assets')
-
-
 def make_dirs():
-    """Creates the directories to store final images and later on, their corresponding json data. If 
+    """Creates the directories to store final images and later on, their corresponding json data. If
     the folders already exist, print to confirm and continue with the program.
     """
+    # TODO: Decompose this logic from the try/except/else statement; unnecessary - hides info
     try:
+        # TODO: Add conditional check for each dir, then create
         os.mkdir('build')
         os.mkdir('build/images')
         os.mkdir('build/json')
@@ -25,8 +26,10 @@ def make_dirs():
 
 
 def join_layers(assets: str) -> list:
-    """Loops through each layer folder, and chooses a layer from each folder based on the given raritiy
-    weights. It then appends all the paths to a list which act as the final layers for the image. 
+    """Loops through each layer folder and chooses
+        a layer from each folder based on the given
+        rarity weights. It then appends all the paths
+        to a list which act as the final layers for the image.
     """
     final_layers = []
 
@@ -38,10 +41,10 @@ def join_layers(assets: str) -> list:
 
         # Sorts images into alphebetical order for use with rarities
         sorted_layers = sorted(os.listdir(layer_path))
-        
+
         # Choose an image from the given subdirectory based on rarities
         img = random.choices(sorted_layers, weights=(layer['rarities']))
-        
+
         # Store each chosen image path to a list
         final_layers.append(os.path.join(layer_path, img[0]))
 
@@ -50,8 +53,9 @@ def join_layers(assets: str) -> list:
 
 
 def create_metadata(description: str, token_name: str, edition: int, final_layers: list):
-    """Takes in some user data, along with the layers of the image and creates a metadata json for the 
-    image. The json object can be used to provide token data to IPFS or third-party websites such as OpenSea.
+    """Takes in some user data, along with the layers of the image
+        and create a metadata json for the image. The json object
+        can be used to provide token data to IPFS or third-party websites such as OpenSea.
     """
 
     metadata = {
@@ -72,8 +76,8 @@ def create_metadata(description: str, token_name: str, edition: int, final_layer
         intemediary_dict = {}
         split_data = layer.split('/')
 
-        intemediary_dict['trait_type']=split_data[-2]
-        intemediary_dict['value']=split_data[-1].replace('.png', '') #
+        intemediary_dict['trait_type'] = split_data[-2]
+        intemediary_dict['value'] = split_data[-1].replace('.png', '')
 
         metadata['attributes'].append(intemediary_dict)
 
@@ -82,7 +86,7 @@ def create_metadata(description: str, token_name: str, edition: int, final_layer
 
 
 def create_image(token_name: str, edition: int, final_layers: list):
-    """Takes a list of final layers, and pastes them all onto the background image to create one 
+    """Takes a list of final layers, and pastes them all onto the background image to create one
     final image. Saves it in the images folder."""
 
     # Sets the background layer
@@ -97,10 +101,11 @@ def create_image(token_name: str, edition: int, final_layers: list):
 
 
 def main():
-    """Takes inputs for the desired images. Creates a build directory, edition counter, then loops 
+    """Takes inputs for the desired images. Creates a build directory, edition counter, then loops
     through for the desired amount. DNA keeps track of each created image to avoid duplicates."""
 
-    token_name = input('Enter the name for your tokens. This will appear as the name for each image\n')
+    token_name = input('Enter the name for your tokens. \
+        This will appear as the name for each image\n')
     description = input('Enter the description for your tokens\n')
     amount = int(input('Enter the amount of images you would like created\n'))
 
@@ -108,21 +113,22 @@ def main():
     dna_set = set()
 
     make_dirs()
+    assets_directory = os.path.join(os.getcwd(), 'assets')
 
     for _ in range(amount):
 
-        final_layers = join_layers(ASSETS)
+        final_layers = join_layers(assets_directory)
 
         if final_layers in dna_set:
-            
+
             print(f'DNA already exists! Retrying token {edition}')
-            continue 
+            continue
 
         create_metadata(description, token_name, edition, final_layers)
         create_image(token_name, edition, final_layers)
         dna_set.add(final_layers)
         edition += 1
-    
+
     print('Image creation complete')
 
 
